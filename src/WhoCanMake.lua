7d348c3b33e7a37e7381d3abab521cafdbed30e7
opussf <github@zz9-za.com>
2026-01-13 19:03:08 -0800

    Localization files, TOC, and base script.

---
+++ src/WhoCanMake.lua
@@ -0,0 +1,4 @@
+WCM_SLUG, WCM   = ...
+WCM.MSG_ADDONNAME = C_AddOns.GetAddOnMetadata( WCM_SLUG, "Title" )
+WCM.MSG_VERSION   = C_AddOns.GetAddOnMetadata( WCM_SLUG, "Version" )
+WCM.MSG_AUTHOR    = C_AddOns.GetAddOnMetadata( WCM_SLUG, "Author" )
